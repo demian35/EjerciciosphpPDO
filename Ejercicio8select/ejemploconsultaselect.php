067e6634f4b19b09b57585c9cea3ad02f42389aa
Demian <53890358+demian35@users.noreply.github.com>
2023-07-22 02:25:21 -0600

    ejemplo de consulta select con pdo

---
+++ Ejercicio8select/ejemploconsultaselect.php
@@ -0,0 +1,25 @@
+<?php
+
+$server="localhost";//nombre del servidor de la bd
+$user="root"; //usuario de la bd
+$password="root"; //contraseña para ingresar a al bd
+$basedatos="basededatospracticapdo";
+
+try{
+    $conn=new PDO("mysql:host=$server;dbname=$basedatos",$user,$password);//creamos la conexion la cual recibe los datos para usar la bd
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Conexion establecida con exito"."<br>";
+    //preparamos una sentencia con el metodo prepare() en ligar de usar ? como en msqli usamos referencias :alumno
+   $consulta=$conn->prepare("SELECT * FROM alumnos");//preparamos la query
+   $consulta->execute();//ejecutamos la query
+
+   $resultados=$consulta->fetchAll();//regresamos los resultados de la query
+
+   foreach($resultados as $alumnos){
+    echo "id :".$alumnos['idalumno']."Nombre : ".$alumnos['alumno']."Correo electronico: ".$alumnos['email']."<br>";
+   }
+}catch(PDOException $error){
+    echo "Error insertar"." ".$error->getMessage();
+}
+
+?>
